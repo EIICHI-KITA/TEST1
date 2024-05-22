07af7ef476651f42435b5ed476b1960200d36bbb
EIICHI-KITA <eiichi.kita.j6s@jp.denso.com>
2024-05-22 10:18:54 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,9 +1,17 @@
-import cv2
-import math
-import matplotlib.pyplot as plt
+import streamlit as st
+import pandas as pd
+ 
+st.write("""
+# My first app
+Hello *world!*
+""")
 
-img = cv2.imread('sample.jpg')
+# import cv2
+# import math
+# import matplotlib.pyplot as plt
 
-img = cv2.cvtColor(img, cv2.COLOR_RGB2RGB)
-plt.imshow(img)
+# img = cv2.imread('sample.jpg')
+
+# img = cv2.cvtColor(img, cv2.COLOR_RGB2RGB)
+# plt.imshow(img)
 
