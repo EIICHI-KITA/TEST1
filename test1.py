a138669b9c002c9d0e955dcbdcbb3aed59b0e257
EIICHI-KITA <eiichi.kita.j6s@jp.denso.com>
2024-05-31 09:52:27 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,26 +1,6 @@
-# from google.colab import files
-# uploaded = files.upload()
-# import cv2 from google.colab.patches import cv2_imshow
-
 import streamlit as st
-import pandas as pd
-# import matplotlib.pyplot as plt
-import mediapipe as mp
-# from mediapipe.tasks import python
-# from mediapipe.tasks.python import vision
 
 st.write("""
 # My first app
 Hello *world!*
 """)
-# plt.imshow('sample.jpg')
-
-# import cv2
-# import math
-# import matplotlib.pyplot as plt
-
-# img = cv2.imread('sample.jpg')
-
-# img = cv2.cvtColor(img, cv2.COLOR_RGB2RGB)
-# plt.imshow(img)
-
